83d6fe9f33c40dbdbeb0a2810cbc50ae823b701a
Juanes <juanesalazar2004@gmail.com>
2025-05-14 16:08:06 -0500

    subpages on dashboard added

---
+++ src/pages/dashboard/soporte/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardLayout from '../layout'
+
+const index = () => {
+    return (
+        <DashboardLayout >
+            <div>index</div>
+        </DashboardLayout>
+    )
+}
+
+export default index
