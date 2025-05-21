12c9059508722fc2bc88fae46f530868238ab80b
Juanes <juanesalazar2004@gmail.com>
2025-05-21 10:17:57 -0500

    rebuild

--- src/pages/dashboard/modulos-capacitacion/index.tsx
+++ src/pages/dashboard/modulos-capacitacion/index.tsx
@@ -271,18 +271,7 @@ const index = () => {
                         <p className="text-gray-900">{recomendacion}</p>
                       </div>
                       
-                      {/* Acciones adicionales */}
-                      <div className="mt-6">
-                        <button
-                          onClick={() => { /* Función para imprimir o guardar resultados */ }}
-                          className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                        >
-                          <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd" />
-                          </svg>
-                          Guardar resultados
-                        </button>
-                      </div>
+                      
                     </div>
                   </div>
                 </div>
